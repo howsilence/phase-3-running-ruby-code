dba5cbc0f8a2765181fcf440daed3eabdf8c749e
howsilence <statton0104@gmail.com>
2022-02-21 15:36:35 -0500

    commit tree 6c6b8894ef315b987702bce89c464148448312b8

---
+++ app.rb
@@ -0,0 +1,10 @@
+# app.rb
+puts "Hello World!"
+puts [1,2,3]
+pp [{id: 1, hello: "World!"}, {id: 2, hello: "Ruby"}, {id:3, hello: "Moon"}], {id:4, hello:"Learner"}
+puts "Hello IRB"
+first_number = 7
+first_number
+puts first_number
+print "Pass this test, please."
+p [1,2,3]
